abd8e5f6119fd1e5c6376c630c3167abb8706c25
Taras Poiatsyka <tvsxar@MacBook-Air---Taras.local>
2025-06-22 18:40:02 +0300

    final version

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000/api/v1";
+const API_URL = import.meta.env.VITE_API_URL;;
 const credentials = {
   email: "zxcq020520@gmail.com",
   password: "admin-password",
